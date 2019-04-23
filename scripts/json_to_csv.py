002dc4c303cdb29629b90d0e49e02c483839ebc7
akshat <aksmit94@gmail.com>
2019-04-23 00:15:05 -0400

    parse json to csv

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -2,21 +2,54 @@ import pandas as pd
 import os.path
 import json
 
-dir = os.path.dirname(os.path.abspath(__file__))
-# data_folder = os.path.join(dir, '..', '..')
-# data_folder = '..\\..\\data\\subset\\charliehebdo'
-data_folder = os.path.join(dir, 'subset', 'charliehebdo')
+base_dir = '/home/akshat/PycharmProjects/FakeTastic'
+data_folder1 = os.path.join(base_dir, 'data')
+data_folder = os.path.join(data_folder1, 'subset', 'charliehebdo')
 rumours = os.path.join(data_folder, 'rumours')
 non_rumours = os.path.join(data_folder, 'non-rumours')
-# curr_file = os.path.join(rumours, '552943855021330432.json')
-dat = dict()
+curr_file = os.path.join(rumours, '552943855021330432.json')
 
-with open(curr_file) as json_file:
-    data = json.load(json_file)
-    dat['id'] = data['id_str']
-    dat['text'] = data['text']
-    dat['in_reply_to_status_id'] = data['in_reply_to_status_id_str']
-    dat['retweeted'] = data['retweeted']
-    dat['retweet_count'] = data['retweet_count']
+# print(os.listdir(non_rumours))
 
-print dat
+# Mimic database table Tweets
+labels = ['tweet_id', 'tweet_text', 'retweet', 'retweet_source_id', 'retweet_count', 'is_fake']
+
+data_list_fake = []
+data_list_non_fake = []
+
+for file in os.listdir(rumours):
+    curr_file = os.path.join(rumours, file)
+    with open(curr_file) as json_file:
+        d = json.load(json_file)
+        id = d['id_str']
+        text = d['text']
+        if d['retweeted'] == False:
+            retweeted = 'false'
+        else:
+            retweeted = 'true'
+        in_reply_to = d['in_reply_to_status_id_str']
+        retweet_count = int(d['retweet_count'])
+        is_fake = 'true'
+        data_list_fake.append([id, text, retweeted, in_reply_to, retweet_count, is_fake])
+
+for file in os.listdir(non_rumours):
+    curr_file = os.path.join(non_rumours, file)
+    with open(curr_file) as json_file:
+        d = json.load(json_file)
+        id = d['id_str']
+        text = d['text']
+        if d['retweeted'] == False:
+            retweeted = 'false'
+        else:
+            retweeted = 'true'
+        in_reply_to = d['in_reply_to_status_id_str']
+        retweet_count = int(d['retweet_count'])
+        is_fake = 'false'
+        data_list_non_fake.append([id, text, retweeted, in_reply_to, retweet_count, is_fake])
+
+all_tweets = data_list_fake + data_list_non_fake
+
+df = pd.DataFrame.from_records(all_tweets, columns=labels)
+
+# write to csv
+df.to_csv(os.path.join(data_folder1, 'tweets.csv'), index=False)
