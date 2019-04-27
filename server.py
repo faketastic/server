273a98da6a1ef997fb9d3d0eb657d03d4d6720bc
andreanr <andrea.navarrete126@gmail.com>
2019-04-27 12:54:39 -0400

    add home html with style

--- server.py
+++ server.py
@@ -90,65 +90,65 @@ def teardown_request(exception):
     pass
 
 
-@app.route('/')
-def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print(request.args)
-
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
+#@app.route('/')
+#def index():
+#  """
+#  request is a special object that Flask provides to access web request information:
+#
+#  request.method:   "GET" or "POST"
+#  request.form:     if the browser submitted a form, this contains the data in the form
+#  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+#
+#  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+#  """
+#
+#  # DEBUG: this is debugging code to see what request looks like
+#  print(request.args)
+#
+#
+#  #
+#  # example of a database query
+#  #
+#  cursor = g.conn.execute("SELECT name FROM test")
+#  names = []
+#  for result in cursor:
+#    names.append(result['name'])  # can also be accessed using result[0]
+#  cursor.close()
+#
+#  #
+#  # Flask uses Jinja templates, which is an extension to HTML where you can
+#  # pass data to a template and dynamically generate HTML based on the data
+#  # (you can think of it as simple PHP)
+#  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
+#  #
+#  # You can see an example template in templates/index.html
+#  #
+#  # context are the variables that are passed to the template.
+#  # for example, "data" key in the context variable defined below will be 
+#  # accessible as a variable in index.html:
+#  #
+#  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+#  #     <div>{{data}}</div>
+#  #     
+#  #     # creates a <div> tag for each element in data
+#  #     # will print: 
+#  #     #
+#  #     #   <div>grace hopper</div>
+#  #     #   <div>alan turing</div>
+#  #     #   <div>ada lovelace</div>
+#  #     #
+#  #     {% for n in data %}
+#  #     <div>{{n}}</div>
+#  #     {% endfor %}
+#  #
+#  context = dict(data = names)
+#
+#
+#  #
+#  # render_template looks in the templates/ folder for files.
+#  # for example, the below file reads template/index.html
+#  #
+#  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at
@@ -170,9 +170,9 @@ def landing():
 
     hashtags = []
     for result in hashtag_cursor:
-        hashtags.append(results[1])
+        hashtags.append(result[1])
 
-    return render_template('landing.html', hahstags=hashtags)
+    return render_template('home.html', hahstags=hashtags)
 
 
 # Example of adding new data to the database
